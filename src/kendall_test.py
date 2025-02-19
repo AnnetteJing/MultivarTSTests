b51e7b9520224b2e871d57e64c4ac2058891378e
Annette Jing <annette.jing@gmail.com>
2025-02-18 17:17:05 -0800

    Add PIT computation and Kendall dist simulation to KendallTest

--- src/kendall_test.py
+++ src/kendall_test.py
@@ -1,35 +1,50 @@
 import math
 import numpy as np
 from scipy.signal import convolve2d
-from scipy.stats._survival import EmpiricalDistributionFunction   # type: ignore
+from scipy.stats import ecdf, rv_continuous
 from typing import Optional
 from collections.abc import Sequence
 from numpy.typing import ArrayLike
+from tqdm.auto import tqdm
 
 
 class KendallTest:
     def __init__(
         self, 
-        V_tilde: ArrayLike, 
-        kendall_dists: Sequence[EmpiricalDistributionFunction],
+        targets: ArrayLike,
+        joint_dists: Sequence[rv_continuous], 
+        marginal_dists: Sequence[rv_continuous],
+        copulas: Sequence[rv_continuous],
+        num_sim: int = 500,
         num_bootstrap: int = 1000,
         block_len: Optional[int] = None,
         grid: Optional[ArrayLike] = None,
+        verbose: bool = True,
     ):
         """
-        V_tilde: [N,] array of partial empirical PITs
-        kendall_dists: Sequence of N Kendall distributions of empirical Copulas
-            kendall_dists[t] is denoted by K_{Hat{C}_t} in the paper
+        targets: [N, D] array of realized forecasting targets
+            Denoted by Y_{t + h}, t = W, ..., T - h = N + W - 1, in the paper
+        joint_dists: Length N list of D-dimensional continuous forecast densities
+            joint_dists[t].cdf(y) = Hat{F}_t(y), t = W, ..., T - h = N + W - 1
+        marginal_dists: Length N list of marginals corresponding to each Hat{F}_t
+            marginal_dists[t].cdf(y) = [Hat{F}_{t, 1}(y_1), ..., Hat{F}_{t, D}(y_D)]
+        copulas: Length N list of copulas corresponding to each Hat{F}_t
+            copulas[t].cdf(u) = Hat{C}_t(u)
+        num_sim (I): Number of samples used for simulating the Kendall distribution of Hat{C}_t
         num_bootstrap (B): Number of bootstrap replications
         block_len (l): Length of each block in the block bootstrap
             Defaults to floor(N**(1/2.1))
         grid: [Z,] array that partitions [0, 1]
+        verbose: Whether to print progress bars, updates, and warnings
         ---
+        V_tilde: [N,] array of partial empirical Kendall variables
+            Tilde{V}_t = Hat{C}_t(Tilde{U}_t), where the partial empirical PITs Tilde{U}_t is 
+            defined by Tilde{U}_{t, d} = Hat{F}_{t, d}(Y_{t + h, d})
+        kendall_dists: [N, Z] array representing the input Kendall distributions,
+            the t-th row corresponds to kendall_dists[t], i.e. K_{Hat{C}_t}
         V_tilde_indicators: [N, Z] array; (t, z)-th element is 1{V_tilde[t] <= grid[z]}
         V_tilde_dist: [Z,] array representing the empirical distribution of V_tilde
             Denoted by Tilde{K}_N in the paper
-        kendall_dists: [N, Z] array representing the input Kendall distributions,
-            the t-th row corresponds to kendall_dists[t], i.e. K_{Hat{C}_t}
         ep: [Z,] array representing the Kendall empirical process
             Denoted by Psi_N in the paper
         ks_stat: Kolmogorov-Smirnov (KS) statistic associated with the Kendall empirical process
@@ -37,26 +52,29 @@ class KendallTest:
         bootstrap_moving_sums: [N - l + 1, Z] array where each row is the inner sum 
             of the bootstrapped process
         """
-        self.num_samples = len(V_tilde) # N
-        self.V_tilde = np.array(V_tilde) # [N,]
+        self.num_samples = targets.shape[0] # N
+        self.targets = np.array(targets) # [N, D]
         self.num_bootstrap = num_bootstrap # B
         self.block_len = math.floor(
             self.num_samples**(1/2.1)
         ) if block_len is None else int(block_len) # l
-        assert self.block_len > 2
+        assert self.block_len > 2, "Not enough data for bootstrapping"
         self.grid = np.linspace(0, 1, 1001) if grid is None else np.array(grid)
+        # Define self.V_tilde & self.kendall_dists
+        self._simulate_kendall(
+            joint_dists=joint_dists, 
+            marginal_dists=marginal_dists, 
+            copulas=copulas,
+            num_sim=num_sim,
+            verbose=verbose,
+        )
         # Calculate 1{V_tilde[t] <= grid[z]} for each t & z
         self.V_tilde_indicators = (
             self.V_tilde[:, np.newaxis] <= self.grid
         ) # [N, Z]
         # Represent the empirical distribution of V_tilde as an array
         self.V_tilde_dist = np.mean(self.V_tilde_indicators, axis=0) # [Z,]
-        # Represent the Kendall distribution of each C_hat_t as a row in the matrix
-        self.kendall_dists = np.array([
-            kendall_t.evaluate(self.grid)
-            for kendall_t in kendall_dists
-        ]) # [N, Z]
-        # Evaluate the Kendall empirical process
+        # Define self.ep
         self._evaluate_process()
         # Calculate test statitics
         self.ks_stat = self._ks_statistic(self.ep)
@@ -65,6 +83,31 @@ class KendallTest:
         self._prepare_bootstrap()
         # Generate bootstrap distributions of the test statistics
         self.generate_bootstrap_dist()
+
+    def _simulate_kendall(
+        self, 
+        joint_dists: Sequence[rv_continuous], 
+        marginal_dists: Sequence[rv_continuous],
+        copulas: Sequence[rv_continuous],
+        num_sim: int,
+        verbose: bool,
+    ) -> None:
+        V_tilde = []
+        kendall_dists = []
+        for t, Yt in enumerate(tqdm(self.targets, disable=not verbose)):
+            # Calculate partially empirical PITs (pePITs)
+            pe_pits_t = marginal_dists[t].cdf(Yt)
+            # Calculate a sample of fully empirical PITs (fePITs)
+            hat_Yt_samp = joint_dists[t].rvs(size=num_sim) # [I, D]
+            fe_pits_t = marginal_dists[t].cdf(hat_Yt_samp) # [I, D]
+            # Copula-transform pePITs
+            V_tilde_t = copulas[t].cdf(pe_pits_t).item()
+            V_tilde.append(V_tilde_t)
+            # Copula-transform fePITs to get empirical Kendall distribution
+            kendall_t = ecdf(copulas[t].cdf(fe_pits_t)).cdf
+            kendall_dists.append(kendall_t.evaluate(self.grid))
+        self.V_tilde = np.array(V_tilde) # [N,]
+        self.kendall_dists = np.array(kendall_dists) # [N, Z]
 
     def _evaluate_process(self) -> None:
         mean_kendall_dist = np.mean(self.kendall_dists, axis=0) # [Z,]
