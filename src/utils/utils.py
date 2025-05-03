fca8e5bfc6878c3c39e84777b688b012998caada
Annette Jing <annette.jing@gmail.com>
2025-05-03 16:33:18 -0700

    Move autocovariance calc to utils, add docstring

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,10 +2,18 @@ import numpy as np
 from numpy.typing import ArrayLike
 from scipy.linalg import inv
 from scipy.stats import f
+from typing import Sequence
 import warnings
 
 
 def matrix_inverse(mat: np.ndarray) -> np.ndarray:
+    """
+    Args:
+        mat ([D, D]): A square matrix to be inverted
+    Returns:
+        mat_inv ([D, D]): Inverse of mat. Uses Moore-Penrose pseudo-inverse
+            if an exact inverse cannot be found.
+    """
     try:
         mat_inv = inv(mat)  # [D, D]
     except (np.linalg.LinAlgError, ValueError):
@@ -30,6 +38,34 @@ def hotelling_t2_ppf(q: ArrayLike, dim_rv: int, samp_size: int) -> ArrayLike:
     f_quantile = f.ppf(q=q, dfn=dim_rv, dfd=dfd)
     t2_quantile = f_quantile * dim_rv * (samp_size - 1) / dfd
     return t2_quantile
+
+
+def get_autocovariance(x: np.ndarray, lags: Sequence[int] | int):
+    """
+    Arguments:
+        x: [D, T] array, with D being the number of variables & T the number of timesteps
+            [T,] arrays will be reshaped to [1, T]
+        lags: Length L sequence of lags of autocovariance to compute
+    Returns:
+        auto_covs: [L, D, D] array of the empirical autocovariances
+            auto_covs[l] = hat{Cov}(X_t, X_{t - l})
+            = Average of outer(x_t - x_mean, x_{t - l} - x_mean) across t = l + 1, ..., T
+    """
+    if isinstance(lags, int):
+        lags = [lags]
+    if len(x.shape) == 1:
+        x = x[np.newaxis, :]
+    x_mean = np.mean(x, axis=1, keepdims=True)  # [D, 1]
+    x_centered = x - x_mean  # [D, T]
+    auto_covs = []
+    for lag in lags:
+        # Empirical estimate of Cov(X_t, X_{t - lag}) (not de-biased)
+        auto_cov_l = np.einsum(
+            "it,jt->ijt", np.roll(x_centered, lag)[:, lag:], x_centered[:, lag:]
+        ).mean(axis=2)  # [D, D]
+        auto_covs.append(auto_cov_l)
+    auto_covs = np.array(auto_covs)  # [L, D, D]
+    return auto_covs
 
 
 def get_unif_grid(num_points: int, num_dims: int) -> np.ndarray:
