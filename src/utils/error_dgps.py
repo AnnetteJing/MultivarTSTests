183c799ebc379ce0fb7d8048a123f55902086907
Annette Jing <annette.jing@gmail.com>
2025-05-03 17:28:23 -0700

    Rename ma_lags to ma_lag

--- src/utils/error_dgps.py
+++ src/utils/error_dgps.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def mariano_preve_dgp(
-    num_timesteps: int, dim: int, ma_lags: int, theta: float, rho: float
+    num_timesteps: int, dim: int, ma_lag: int, theta: float, rho: float
 ) -> np.ndarray:
     """
     Args:
         num_timesteps (T)
         dim (D)
-        ma_lags (q)
+        ma_lag (q)
         theta: Parameter controlling strength of serial correlation
         rho: Parameter controlling strength of contemporaneous correlation
     Returns:
@@ -18,15 +18,15 @@ def mariano_preve_dgp(
     eps_cov = rho * np.ones((dim, dim))
     np.fill_diagonal(eps_cov, 1.0)
     eps = np.random.multivariate_normal(
-        mean=np.zeros(dim), cov=eps_cov, size=num_timesteps + ma_lags
+        mean=np.zeros(dim), cov=eps_cov, size=num_timesteps + ma_lag
     )  # [T + q, D]
     ma_coef = np.kron(
-        theta ** np.arange(1, ma_lags + 1), 1 / np.sqrt(np.arange(1, dim + 1))
+        theta ** np.arange(1, ma_lag + 1), 1 / np.sqrt(np.arange(1, dim + 1))
     ).reshape(-1, dim)  # [q, D]
     ma_coef = np.vstack((np.ones(dim), ma_coef))  # [q + 1, D]
     # Calculate moving average of the white noise series
     eps_windows = np.lib.stride_tricks.sliding_window_view(
-        eps, window_shape=ma_lags + 1, axis=0
+        eps, window_shape=ma_lag + 1, axis=0
     )  # [T, D, q + 1]
     ma_series = np.einsum("tdj,jd->dt", eps_windows, ma_coef)  # [D, T]
     return ma_series
